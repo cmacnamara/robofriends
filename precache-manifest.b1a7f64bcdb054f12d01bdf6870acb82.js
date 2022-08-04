020ede175ed2f6acaa888b89972d8dcffa36ee8c
Chris MacNamara <cpmacnamara@gmail.com>
2022-08-04 14:02:56 -0400

    Updates

---
+++ precache-manifest.b1a7f64bcdb054f12d01bdf6870acb82.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "891828c654c4004be031",
+    "url": "/robofriends/static/css/main.a6b1cd2e.chunk.css"
+  },
+  {
+    "revision": "891828c654c4004be031",
+    "url": "/robofriends/static/js/main.854047b3.chunk.js"
+  },
+  {
+    "revision": "75dcfc39fa8879b648fd",
+    "url": "/robofriends/static/js/runtime~main.e6791cfe.js"
+  },
+  {
+    "revision": "5513d91e180cb0c97357",
+    "url": "/robofriends/static/css/2.1547b228.chunk.css"
+  },
+  {
+    "revision": "5513d91e180cb0c97357",
+    "url": "/robofriends/static/js/2.fd0ca411.chunk.js"
+  },
+  {
+    "revision": "440f8641b4ac1935df7a",
+    "url": "/robofriends/static/js/3.76c3d8c6.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "c153bce98e7b365d23114d60533c8a5e",
+    "url": "/robofriends/index.html"
+  }
+];
